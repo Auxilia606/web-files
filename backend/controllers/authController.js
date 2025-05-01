e854891ddc64ca93ee3001be07af4c99c564469c
auxilia <auxilia606@gmail.com>
2025-05-02 00:36:26 +0900

    Feat: 회원가입, 로그인의 이메일 아이디를 일반 아이디로 변경

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,25 +5,25 @@ const bcrypt = require("bcryptjs");
 // 토큰 생성 함수
 const generateAccessToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN, // 보통 15분
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
 const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN, // 보통 7일
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
   });
 };
 
 // 회원가입
 exports.register = async (req, res) => {
-  const { email, password } = req.body;
+  const { loginId, password } = req.body;
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await db.execute("INSERT INTO users (email, password) VALUES (?, ?)", [
-      email,
+    await db.execute("INSERT INTO users (login_id, password) VALUES (?, ?)", [
+      loginId,
       hashedPassword,
     ]);
 
@@ -35,40 +35,40 @@ exports.register = async (req, res) => {
 
 // 아이디 중복 확인
 exports.checkId = async (req, res) => {
-  const { email } = req.body;
+  const { loginId } = req.body;
 
-  if (!email) {
-    return res.status(400).json({ message: "이메일을 입력해주세요" });
+  if (!loginId) {
+    return res.status(400).json({ message: "아이디를 입력해주세요" });
   }
 
   try {
-    const [rows] = await db.execute("SELECT id FROM users WHERE email = ?", [
-      email,
+    const [rows] = await db.execute("SELECT id FROM users WHERE loginId = ?", [
+      loginId,
     ]);
 
     if (rows.length > 0) {
-      return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
+      return res.status(409).json({ message: "이미 존재하는 아이디입니다." });
     }
 
-    return res.status(200).json({ message: "사용 가능한 이메일입니다." });
+    return res.status(200).json({ message: "사용 가능한 아이디입니다." });
   } catch {
-    res.status(500).json({ message: "이메일 확인 실패" });
+    res.status(500).json({ message: "아이디 확인 실패" });
   }
 };
 
 // 로그인
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
+  const { loginId, password } = req.body;
 
   try {
-    const [users] = await db.execute("SELECT * FROM users WHERE email = ?", [
-      email,
+    const [users] = await db.execute("SELECT * FROM users WHERE loginId = ?", [
+      loginId,
     ]);
 
     if (users.length === 0) {
       return res
         .status(401)
-        .json({ message: "이메일 또는 비밀번호가 틀립니다." });
+        .json({ message: "아이디 또는 비밀번호가 틀립니다." });
     }
 
     const user = users[0];
@@ -77,7 +77,7 @@ exports.login = async (req, res) => {
     if (!isMatch) {
       return res
         .status(401)
-        .json({ message: "이메일 또는 비밀번호가 틀립니다." });
+        .json({ message: "아이디 또는 비밀번호가 틀립니다." });
     }
 
     const accessToken = generateAccessToken(user.id);
